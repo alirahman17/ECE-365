71be3c5caf02bc46f8dca2e0cbb27cb33c87f489
alirahman17 <alirahman1117@gmail.com>
2018-10-18 17:41:04 -0400

    Updated Functions

--- 2/heap.cpp
+++ 2/heap.cpp
@@ -10,7 +10,7 @@ heap::heap(int capacity){
 int heap::insert(const std::string &id, int key, void *pv){
   if(!(filled < capacity))
     return 1;
-  else if(map->contains(id))
+  else if(mapping->contains(id))
     return 2;
   else{
     node tmp;
@@ -50,7 +50,18 @@ int heap::deleteMin(std::string *pId, int *pKey, void *ppData){
   if(!filled)
     return 1;
   else{
+    if(pId != nullptr)
+      *pId = data[1].id;
+    if(pKey != nullptr)
+      *pKey = data[1].key;
+    if(ppData != nullptr)
+      *(void **)ppData = data[1].pv;
 
+    mapping->remove(data[1].id);
+    data[1] = data[filled--];
+    percolateDown(1);
+
+    return 0;
   }
 }
 
@@ -58,7 +69,25 @@ int heap::remove(const std::string &id, int *pKey, void *ppData){
   if(!(mapping->contains(id)))
     return 1;
   else{
+    node *tmp = (node *)mapping->getPointer(id);
+    int i = getPos(tmp);
 
+    if(pKey != nullptr)
+      *pKey = tmp->key;
+    if(ppData != nullptr)
+      *(void **)ppData = tmp->pv;
+
+    mapping->remove(id);
+    int oldKey = tmp->key;
+    data[i] = data[filled--];
+    int newKey = tmp->key;
+
+    if(newKey > oldKey)
+      percolateDown(i);
+    else if(oldKey > newKey)
+      percolateUp(i);
+
+    return 0;
   }
 }
 
